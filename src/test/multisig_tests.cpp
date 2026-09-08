9986017a386289ec24c0b32673d977c01ed99595
gwestcot <34328160+gwestcot@users.noreply.github.com>
2026-09-07 19:59:41 -0700

    test: adapt multisig tests to Mercatura signature rules

--- src/test/multisig_tests.cpp
+++ src/test/multisig_tests.cpp
@@ -39,32 +39,59 @@ sign_multisig(const CScript& scriptPubKey, const std::vector<CKey>& keys, const 
 
 BOOST_AUTO_TEST_CASE(multisig_verify)
 {
-    script_verify_flags flags = SCRIPT_VERIFY_P2SH | SCRIPT_VERIFY_STRICTENC;
+    // Mercatura disables inherited ECDSA/Schnorr ownership authorization.
+    // CHECKMULTISIG scripts may still be parsed/classified, but execution
+    // must fail closed rather than authorize ownership.
+
+    const script_verify_flags flags{
+        SCRIPT_VERIFY_P2SH | SCRIPT_VERIFY_STRICTENC
+    };
 
     ScriptError err;
     CKey key[4];
-    CAmount amount = 0;
-    for (int i = 0; i < 4; i++)
-        key[i].MakeNewKey(true);
+    CAmount amount{0};
 
-    CScript a_and_b;
-    a_and_b << OP_2 << ToByteVector(key[0].GetPubKey()) << ToByteVector(key[1].GetPubKey()) << OP_2 << OP_CHECKMULTISIG;
+    for (auto& k : key) {
+        k.MakeNewKey(true);
+    }
 
-    CScript a_or_b;
-    a_or_b << OP_1 << ToByteVector(key[0].GetPubKey()) << ToByteVector(key[1].GetPubKey()) << OP_2 << OP_CHECKMULTISIG;
+    const CScript a_and_b{
+        CScript()
+            << OP_2
+            << ToByteVector(key[0].GetPubKey())
+            << ToByteVector(key[1].GetPubKey())
+            << OP_2
+            << OP_CHECKMULTISIG
+    };
 
-    CScript escrow;
-    escrow << OP_2 << ToByteVector(key[0].GetPubKey()) << ToByteVector(key[1].GetPubKey()) << ToByteVector(key[2].GetPubKey()) << OP_3 << OP_CHECKMULTISIG;
+    const CScript a_or_b{
+        CScript()
+            << OP_1
+            << ToByteVector(key[0].GetPubKey())
+            << ToByteVector(key[1].GetPubKey())
+            << OP_2
+            << OP_CHECKMULTISIG
+    };
 
-    CMutableTransaction txFrom;  // Funding transaction
+    const CScript escrow{
+        CScript()
+            << OP_2
+            << ToByteVector(key[0].GetPubKey())
+            << ToByteVector(key[1].GetPubKey())
+            << ToByteVector(key[2].GetPubKey())
+            << OP_3
+            << OP_CHECKMULTISIG
+    };
+
+    CMutableTransaction txFrom;
     txFrom.vout.resize(3);
     txFrom.vout[0].scriptPubKey = a_and_b;
     txFrom.vout[1].scriptPubKey = a_or_b;
     txFrom.vout[2].scriptPubKey = escrow;
 
-    CMutableTransaction txTo[3]; // Spending transaction
-    for (int i = 0; i < 3; i++)
-    {
+    CMutableTransaction txTo[3];
+
+    for (int i = 0; i < 3; ++i) {
         txTo[i].vin.resize(1);
         txTo[i].vout.resize(1);
         txTo[i].vin[0].prevout.n = i;
@@ -72,69 +99,51 @@ BOOST_AUTO_TEST_CASE(multisig_verify)
         txTo[i].vout[0].nValue = 1;
     }
 
-    std::vector<CKey> keys;
-    CScript s;
+    // Use historically sufficient Bitcoin signature sets. Mercatura must
+    // reject them specifically because CHECKMULTISIG authorization is
+    // disabled, not because signatures are absent.
+    const std::vector<std::vector<CKey>> signing_keys{
+        {key[0], key[1]},
+        {key[0]},
+        {key[0], key[1]},
+    };
 
-    // Test a AND b:
-    keys.assign(1,key[0]);
-    keys.push_back(key[1]);
-    s = sign_multisig(a_and_b, keys, CTransaction(txTo[0]), 0);
-    BOOST_CHECK(VerifyScript(s, a_and_b, nullptr, flags, MutableTransactionSignatureChecker(&txTo[0], 0, amount, MissingDataBehavior::ASSERT_FAIL), &err));
-    BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_OK, ScriptErrorString(err));
+    const CScript scripts[]{
+        a_and_b,
+        a_or_b,
+        escrow,
+    };
 
-    for (int i = 0; i < 4; i++)
-    {
-        keys.assign(1,key[i]);
-        s = sign_multisig(a_and_b, keys, CTransaction(txTo[0]), 0);
-        BOOST_CHECK_MESSAGE(!VerifyScript(s, a_and_b, nullptr, flags, MutableTransactionSignatureChecker(&txTo[0], 0, amount, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("a&b 1: %d", i));
-        BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_INVALID_STACK_OPERATION, ScriptErrorString(err));
+    for (int i = 0; i < 3; ++i) {
+        const CScript script_sig{
+            sign_multisig(
+                scripts[i],
+                signing_keys[i],
+                CTransaction(txTo[i]),
+                0)
+        };
 
-        keys.assign(1,key[1]);
-        keys.push_back(key[i]);
-        s = sign_multisig(a_and_b, keys, CTransaction(txTo[0]), 0);
-        BOOST_CHECK_MESSAGE(!VerifyScript(s, a_and_b, nullptr, flags, MutableTransactionSignatureChecker(&txTo[0], 0, amount, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("a&b 2: %d", i));
-        BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_EVAL_FALSE, ScriptErrorString(err));
+        err = SCRIPT_ERR_OK;
+
+        BOOST_CHECK(!VerifyScript(
+            script_sig,
+            scripts[i],
+            nullptr,
+            flags,
+            MutableTransactionSignatureChecker(
+                &txTo[i],
+                0,
+                amount,
+                MissingDataBehavior::ASSERT_FAIL),
+            &err));
+
+        BOOST_CHECK_MESSAGE(
+            err == SCRIPT_ERR_BAD_OPCODE,
+            strprintf(
+                "classical multisig form %d: expected BAD_OPCODE, got %s",
+                i,
+                ScriptErrorString(err)));
     }
-
-    // Test a OR b:
-    for (int i = 0; i < 4; i++)
-    {
-        keys.assign(1,key[i]);
-        s = sign_multisig(a_or_b, keys, CTransaction(txTo[1]), 0);
-        if (i == 0 || i == 1)
-        {
-            BOOST_CHECK_MESSAGE(VerifyScript(s, a_or_b, nullptr, flags, MutableTransactionSignatureChecker(&txTo[1], 0, amount, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("a|b: %d", i));
-            BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_OK, ScriptErrorString(err));
-        }
-        else
-        {
-            BOOST_CHECK_MESSAGE(!VerifyScript(s, a_or_b, nullptr, flags, MutableTransactionSignatureChecker(&txTo[1], 0, amount, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("a|b: %d", i));
-            BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_EVAL_FALSE, ScriptErrorString(err));
-        }
-    }
-    s.clear();
-    s << OP_0 << OP_1;
-    BOOST_CHECK(!VerifyScript(s, a_or_b, nullptr, flags, MutableTransactionSignatureChecker(&txTo[1], 0, amount, MissingDataBehavior::ASSERT_FAIL), &err));
-    BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_SIG_DER, ScriptErrorString(err));
-
-
-    for (int i = 0; i < 4; i++)
-        for (int j = 0; j < 4; j++)
-        {
-            keys.assign(1,key[i]);
-            keys.push_back(key[j]);
-            s = sign_multisig(escrow, keys, CTransaction(txTo[2]), 0);
-            if (i < j && i < 3 && j < 3)
-            {
-                BOOST_CHECK_MESSAGE(VerifyScript(s, escrow, nullptr, flags, MutableTransactionSignatureChecker(&txTo[2], 0, amount, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("escrow 1: %d %d", i, j));
-                BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_OK, ScriptErrorString(err));
-            }
-            else
-            {
-                BOOST_CHECK_MESSAGE(!VerifyScript(s, escrow, nullptr, flags, MutableTransactionSignatureChecker(&txTo[2], 0, amount, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("escrow 2: %d %d", i, j));
-                BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_EVAL_FALSE, ScriptErrorString(err));
-            }
-        }
 }
 
 BOOST_AUTO_TEST_CASE(multisig_IsStandard)
@@ -183,44 +192,72 @@ BOOST_AUTO_TEST_CASE(multisig_IsStandard)
 
 BOOST_AUTO_TEST_CASE(multisig_Sign)
 {
-    // Test SignSignature() (and therefore the version of Solver() that signs transactions)
+    // The inherited transaction signer must not produce ownership
+    // authorization for classical CHECKMULTISIG outputs in Mercatura.
     FillableSigningProvider keystore;
     CKey key[4];
-    for (int i = 0; i < 4; i++)
-    {
-        key[i].MakeNewKey(true);
-        BOOST_CHECK(keystore.AddKey(key[i]));
+
+    for (auto& k : key) {
+        k.MakeNewKey(true);
+        BOOST_CHECK(keystore.AddKey(k));
     }
 
-    CScript a_and_b;
-    a_and_b << OP_2 << ToByteVector(key[0].GetPubKey()) << ToByteVector(key[1].GetPubKey()) << OP_2 << OP_CHECKMULTISIG;
+    const CScript a_and_b{
+        CScript()
+            << OP_2
+            << ToByteVector(key[0].GetPubKey())
+            << ToByteVector(key[1].GetPubKey())
+            << OP_2
+            << OP_CHECKMULTISIG
+    };
 
-    CScript a_or_b;
-    a_or_b  << OP_1 << ToByteVector(key[0].GetPubKey()) << ToByteVector(key[1].GetPubKey()) << OP_2 << OP_CHECKMULTISIG;
+    const CScript a_or_b{
+        CScript()
+            << OP_1
+            << ToByteVector(key[0].GetPubKey())
+            << ToByteVector(key[1].GetPubKey())
+            << OP_2
+            << OP_CHECKMULTISIG
+    };
 
-    CScript escrow;
-    escrow << OP_2 << ToByteVector(key[0].GetPubKey()) << ToByteVector(key[1].GetPubKey()) << ToByteVector(key[2].GetPubKey()) << OP_3 << OP_CHECKMULTISIG;
+    const CScript escrow{
+        CScript()
+            << OP_2
+            << ToByteVector(key[0].GetPubKey())
+            << ToByteVector(key[1].GetPubKey())
+            << ToByteVector(key[2].GetPubKey())
+            << OP_3
+            << OP_CHECKMULTISIG
+    };
 
-    CMutableTransaction txFrom;  // Funding transaction
+    CMutableTransaction txFrom;
     txFrom.vout.resize(3);
     txFrom.vout[0].scriptPubKey = a_and_b;
     txFrom.vout[1].scriptPubKey = a_or_b;
     txFrom.vout[2].scriptPubKey = escrow;
 
-    CMutableTransaction txTo[3]; // Spending transaction
-    for (int i = 0; i < 3; i++)
-    {
+    CMutableTransaction txTo[3];
+
+    for (int i = 0; i < 3; ++i) {
         txTo[i].vin.resize(1);
         txTo[i].vout.resize(1);
         txTo[i].vin[0].prevout.n = i;
         txTo[i].vin[0].prevout.hash = txFrom.GetHash();
         txTo[i].vout[0].nValue = 1;
-    }
 
-    for (int i = 0; i < 3; i++)
-    {
         SignatureData empty;
-        BOOST_CHECK_MESSAGE(SignSignature(keystore, CTransaction(txFrom), txTo[i], 0, SIGHASH_ALL, empty), strprintf("SignSignature %d", i));
+
+        BOOST_CHECK_MESSAGE(
+            !SignSignature(
+                keystore,
+                CTransaction(txFrom),
+                txTo[i],
+                0,
+                SIGHASH_ALL,
+                empty),
+            strprintf(
+                "classical CHECKMULTISIG signer unexpectedly succeeded for output %d",
+                i));
     }
 }
 
