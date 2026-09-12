f2ab951f2d019887a19f05dae71165c54ec78fa9
gwestcot <34328160+gwestcot@users.noreply.github.com>
2026-09-11 20:06:33 -0700

    consensus: separate block index subsidy helpers

--- src/consensus/mercatura_controller.cpp
+++ src/consensus/mercatura_controller.cpp
@@ -2,7 +2,6 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <chain.h>
 #include <consensus/mercatura_controller.h>
 
 #include <consensus/mercatura_emission.h>
@@ -337,57 +336,5 @@ std::optional<McaEmissionState> DeriveMcaEmissionState(
         command->subsidy);
 }
 
-std::optional<CAmount> GetMcaBlockSubsidy(
-    const CBlockIndex& block)
-{
-    if (block.nHeight <= 0 ||
-        !block.m_mca_emission_state) {
-        return std::nullopt;
-    }
-
-    const McaEmissionState& state{
-        *block.m_mca_emission_state};
-
-    if (state.height != block.nHeight ||
-        state.subsidy <= 0 ||
-        state.subsidy > MAX_MONEY) {
-        return std::nullopt;
-    }
-
-    return state.subsidy;
-}
-
-std::optional<CAmount> GetNextMcaBlockSubsidy(
-    const CBlockIndex& parent)
-{
-    if (parent.nHeight < 0 ||
-        parent.nHeight == std::numeric_limits<int>::max()) {
-        return std::nullopt;
-    }
-
-    const int height{parent.nHeight + 1};
-    const McaEmissionState* emission_parent{nullptr};
-
-    if (height > 1) {
-        if (!parent.m_mca_emission_state ||
-            parent.m_mca_emission_state->height != parent.nHeight) {
-            return std::nullopt;
-        }
-
-        emission_parent =
-            &*parent.m_mca_emission_state;
-    }
-
-    const auto command{
-        GetMcaEmissionCommand(
-            emission_parent,
-            height)};
-
-    if (!command) {
-        return std::nullopt;
-    }
-
-    return command->subsidy;
-}
 
 } // namespace Consensus
